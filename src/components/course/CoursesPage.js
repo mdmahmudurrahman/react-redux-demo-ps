985225392aa9810987f3d5a80ebf22d68eb30099
gittiggit <contactwithmahmud@gmail.com>
2016-12-02 19:36:19 +0600

    configure store, create reducer, create actions, use connect

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,13 +1,68 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import courseActions from "../../actions/courseActions";
 
 class CoursesPage extends Component {
+  constructor(props, context) {
+    super(props, context);
+    
+    this.state = {
+      course: { title: "" }
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.titleOnChange = this.titleOnChange.bind(this);
+    this.onClickSubmit = this.onClickSubmit.bind(this);
+    // console.log(this.state.course);
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();
+    this.textInput.value = this.textInput1.value;
+  }
+  
+  titleOnChange(e) {
+    const course = this.state.course;
+    course.title = e.target.value;
+    this.setState({course: course});
+  }
+
+  onClickSubmit() {
+    this.props.dispatch(courseActions(this.state.course));
+  }
+
   render() {
     return(
       <div>
-        <h2>Courses</h2>
+        <h3>Courses</h3>
+        <h4>Add Course</h4>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" 
+          value={this.state.course.title} 
+          ref={input => {this.textInput1 = input; }}
+          onChange={this.titleOnChange} />
+
+          <input type="submit" 
+            className="btn btn-info" 
+            onClick={this.onClickSubmit} />
+
+          <input type="text" value={this.state.course.title} readOnly />
+          <input type="text" ref={input => {this.textInput = input; }} readOnly />
+        </form>
       </div>
     );
   }
 }
 
-export default CoursesPage;
+function mapStateToProps(state, ownProps) {
+  console.log(state);
+  return {
+    courses: state.courses
+  };
+}
+
+// function mapDispatchToProps() {
+
+// }
+
+
+export default connect(mapStateToProps)(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import 'babel-polyfill'; //there are a set of features in es6
 // that babel can't transpile, so for those you do need to use 
 // 'babel-polyfill'
+import { Provider } from "react-redux";
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import configureStore from "./store/configureStore"; 
 import routes from './routes';
 import './styles/styles.css'; //Webpack can import css file too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes} />,
-    document.getElementById('app')
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.getElementById('app')
 );
